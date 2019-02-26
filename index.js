fdac3535084ee72d911469be33b8e274e6f4fb6e
hilarious-state-5373 <jacob.lam.work@gmail.com>
2019-02-26 17:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,3 @@ height = parseInt(height)
 module.exports = { name, height
 }
 
-if (name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
-}
